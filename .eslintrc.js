9acaa0e07a61128df2169951ee9aca919f5120dc
Narongsak <54994033+sscarry2@users.noreply.github.com>
2020-07-07 02:37:40 +0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // Standard JavaScript Style Guide
+    extends: ["standard", "plugin:prettier/recommended"],
+    rules: {
+       "prettier/prettier": ["error", { "singleQuote": true }]
+    }
+ };
---
+++ index.js
@@ -0,0 +1,212 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('config')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const auth = require('../assignment-backend/middleware/auth')
+const { check, validationResult } = require('express-validator')
+mongoose.Promise = global.Promise
+mongoose.connect(config.get('mongo.url'),{
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true
+})
+
+const { Schema } = mongoose
+const { ObjectId } = mongoose.Types;
+ObjectId.prototype.valueOf = function () {
+	return this.toString()
+}
+
+const User = mongoose.model('User', new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  firstname: {
+    type: String,
+    required: true
+  },
+  lastname: {
+    type: String,
+    required: true
+  }
+}))
+
+const Transaction = mongoose.model('Transaction', new Schema({
+  user: { type: ObjectId, ref: 'User' },
+  amount: Number,
+  type: String, // income, expense
+  remark: String,
+  date: Date,
+  // balance: Number,
+}))
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => res.send('hello'))
+
+app.get('/users/', auth,(req, res) => {
+  try{
+    User.findById(req.user.id).select('-password')
+    .then((data) => {
+      res.send(data)
+    })
+  }
+  catch(err){
+    console.error(err);
+    res.status(500).send('Server error')
+  }
+})
+
+app.post('/users/login', [
+  check('username', 'Username is Required'),
+  check(
+    'password', 
+    'Password is required'
+  ).exists()
+    .not()
+    .isEmpty()] ,
+ async (req, res) => { 
+      const errors = validationResult(req);
+      if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const { username, password } = req.body;
+      try{
+        let user = await User.findOne({ username });
+        if(!user){
+          return res.status(400).json({ errors: [{ msg: 'Invalid Credentials'}] });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+          return res.status(400).json({ errors: [{ msg: 'Invalid Credentials'}] });
+        }
+        const payload = {
+          user: { 
+            id: user.id
+          }
+        };
+        jwt.sign(
+          payload, 
+          config.get('mongo.jwtSecret'),
+          { expiresIn: 360000 }, 
+          (err, token) => {
+            if(err) {
+              console.error(err);
+            }
+            else{
+              res.json({ token });
+            }
+          }
+        );
+      }
+      catch(err){
+        console.error(err.messasge);
+        res.status(500).send('Server error');
+      }
+})
+
+app.post('/users', [
+  check('firstname', 'FisrtName is required')
+    .not()
+    .isEmpty(),
+  check('lastname', 'LastName is required')
+    .not()
+    .isEmpty(),
+  check(
+    'password', 
+    'Please enter a password with 6 or more characters'
+  ).isLength({ min:6 }),
+  check('username', 'Username is Required')
+    .not()
+    .isEmpty()] ,
+async (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { username, password, firstname, lastname } = req.body;
+  try{
+    let user = await User.findOne({ username });
+    if(user){
+      return res.status(400).json({ errors: [{ msg: 'User already exists'}] });
+    }
+
+    user = new User({
+      username,
+      password,
+      firstname,
+      lastname
+    });
+    const salt  = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+    await user.save();
+    const payload = {
+      user: { 
+        id: user.id
+      }
+    };
+    jwt.sign(
+      payload, 
+      config.get('mongo.jwtSecret'),
+      { expiresIn: 360000 }, 
+      (err, token) => {
+        if(err) {
+          console.error(err);
+        }
+        else{
+          res.json({ token });
+        }
+      }
+    );
+  }
+  catch(err){
+    console.error(err.messasge);
+    res.status(500).send('Server error');
+  }
+  
+});
+
+app.get('/transactions', (req, res) => {
+  Transaction.find({ user: req.query.user })
+    .then((data) => {
+      res.send(data)
+    })
+})
+
+app.post('/transactions', (req, res) => {
+  Transaction.create(req.body)
+    .then((data) => {
+      res.send(data)
+    })
+})
+
+app.put('/transactions/:id', (req, res) => {
+  Transaction.findOneAndUpdate({ _id: req.params.id }, req.body)
+    .then((data) => {
+      res.send(data)
+    })
+})
+
+app.delete('/transactions/:id', (req, res) => {
+  Transaction.findOneAndRemove({ _id: req.params.id })
+    .then((data) => {
+      res.send(data)
+    })
+})
+
+app.listen(8080, () => {
+  console.log('listen on port 8080')
+})
